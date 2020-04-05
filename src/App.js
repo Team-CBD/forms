f57d79da88627e6c4d6a4feec5f6bc74f14a247e
philipipara <54330369+philipipara@users.noreply.github.com>
2020-04-04 20:41:29 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import TripData from "./components/Trips";
+
+function App() {
+  return (
+    <div className="App">
+      <TripData />
+    </div>
+  );
+}
+
+export default App;
